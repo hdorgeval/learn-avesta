9e7ee8a72fc464f8f557e2dde3612cf12a156d7d
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-28 00:23:03 +0100

    feat(app): extract carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { FC } from 'react';
 import './App.css';
 import landingPageVideo from './assets/videos/course-video.mp4';
-import { LetterA, LetterB, LetterC, LetterD } from './components/letters';
+import { LettersCarousel } from './components/carousel-of-letters/LettersCarousel';
 
 export const App: FC = () => {
   return (
@@ -43,72 +43,7 @@ export const App: FC = () => {
               <div className="text-uppercase fw-bold">Discover more</div>
               <div className="fs-7">(will be available soon)</div>
             </button>
-            <div
-              id="carouselExampleIndicators"
-              className="carousel slide mt-5 pb-5"
-              data-bs-ride="carousel"
-            >
-              <div className="carousel-indicators">
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="0"
-                  className="active"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="1"
-                  aria-label="Slide 2"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="2"
-                  aria-label="Slide 3"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="3"
-                  aria-label="Slide 4"
-                ></button>
-              </div>
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <LetterA fill="#f5a425" />
-                </div>
-                <div className="carousel-item">
-                  <LetterB fill="#f5a425" />
-                </div>
-                <div className="carousel-item">
-                  <LetterC fill="#f5a425" />
-                </div>
-                <div className="carousel-item">
-                  <LetterD fill="#f5a425" />
-                </div>
-              </div>
-              <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev"
-              >
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-              </button>
-              <button
-                className="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next"
-              >
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-              </button>
-            </div>
+            <LettersCarousel />
           </div>
         </div>
       </section>
@@ -127,13 +62,14 @@ export const App: FC = () => {
           </a>
         </div>
       </footer>
-      {/* <div>
+      {/* <div className="bg-black">
         <LetterC fill="#f5a425" />
         <LetterA fill="#f5a425" />
         <LetterB fill="#f5a425" />
         <LetterC fill="#f5a425" />
         <LetterB fill="#f5a425" />
         <LetterD fill="#f5a425" />
+        <LetterE fill="#f5a425" />
       </div> */}
     </>
   );
---
+++ src/components/carousel-of-letters/LettersCarousel.tsx
@@ -0,0 +1,83 @@
+import { LetterA, LetterB, LetterC, LetterD, LetterE } from '../letters';
+
+export const LettersCarousel: React.FC = () => {
+  return (
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide mt-5 pb-5"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="4"
+          aria-label="Slide 5"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <LetterA fill="#f5a425" />
+        </div>
+        <div className="carousel-item">
+          <LetterB fill="#f5a425" />
+        </div>
+        <div className="carousel-item">
+          <LetterC fill="#f5a425" />
+        </div>
+        <div className="carousel-item">
+          <LetterD fill="#f5a425" />
+        </div>
+        <div className="carousel-item">
+          <LetterE fill="#f5a425" />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+LettersCarousel.displayName = 'LettersCarousel';
