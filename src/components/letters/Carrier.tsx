0cd1df9cad8d5c6249b0adb38d2ea7d216795415
hdorgeval <henri.dorgeval@outlook.fr>
2021-12-28 17:42:14 +0100

    chore(avesta): add letter 'g'

--- src/components/letters/Carrier.tsx
+++ src/components/letters/Carrier.tsx
@@ -1,9 +1,11 @@
-import { LetterA, LetterB, LetterC, LetterD, LetterE, LetterF } from '.';
+import { LetterA, LetterB, LetterC, LetterD, LetterE, LetterF, LetterG } from '.';
 
 export const Carrier: React.FC = () => {
   return (
     <div className="bg-dark">
       <LetterC fill="#f5a425" />
+      <LetterA fill="#f5a425" />
+      <LetterG fill="#f5a425" />
       <LetterA fill="#f5a425" />
       <LetterB fill="#f5a425" />
       <LetterC fill="#f5a425" />
