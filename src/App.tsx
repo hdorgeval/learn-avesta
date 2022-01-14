89ef3800e36f441c2fcd22d9e6c0fcc562c45767
hdorgeval <henri.dorgeval@outlook.fr>
2022-01-14 22:51:13 +0100

    fix(video): add credit

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,20 @@ export const App: FC = () => {
             <div className="mt-4">
               <VideoPlayer 
                 thumbnailUrl={previewAvestaScriptAndSounds}
-                videoUrl="https://spw.uni-goettingen.de/projects/aig/mp4/AVE-INT-003.mp4" startTime="18:35" />
+                videoUrl="https://spw.uni-goettingen.de/projects/aig/mp4/AVE-INT-003.mp4" startTime="18:35" 
+              />
+              <div className="d-flex flex-column align-items-center">
+                <a
+                  className="text-light fs-6 text-muted"
+                  href="https://spw.uni-goettingen.de/projects/aig/lng-ave.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Video session source"
+                >
+                  Credit: spw.uni-goettingen.de/projects/aig/lng-ave.html
+                  <i className="bi bi-box-arrow-up-right ms-2"></i>
+                </a>
+              </div>
             </div>
           </div>
         </section>
